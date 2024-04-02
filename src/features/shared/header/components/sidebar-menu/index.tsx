4458c5c0ace2aad54ac0bcd0a0baec1ff5f5b40b
am1t-goswami <mern.amitkumar@gmail.com>
2024-04-03 03:59:00 +0530

    ref: code ref

--- src/features/shared/header/components/sidebar-menu/index.tsx
+++ src/features/shared/header/components/sidebar-menu/index.tsx
@@ -41,47 +41,42 @@ export const SideBarMenu = ({
         onClick={() => setSideBarOpen(true)}
         aria-hidden="true"
       />
-      {sideBarOpen && (
-        <Container
-          className={`fixed inset-y-0 right-0 lg:w-4/12 md:w-2/3 sm:w-6/12 bg-gray-900 z-50 transform transition-transform ${sideBarOpen ? 'translate-x-0' : 'translate-x-full'}`}
-        >
-          <Container className="bg-gray-100 h-screen">
-            <Container className="p-7">
-              <Container className="flex items-center justify-between">
-                <Logo />
-                <Button
-                  type="button"
-                  className="text-gray-700"
-                  onClick={() => setSideBarOpen(false)}
-                  btnText={
-                    <React.Fragment>
-                      <Text className="sr-only">Close menu</Text>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </React.Fragment>
-                  }
-                />
-              </Container>
-              <Container className="mt-6">
-                <Container className="divide-y divide-gray-500/10">
-                  <Container className="space-y-2 py-6">
-                    <NavBarOptions
-                      navigationData={navigationData}
-                      user={user}
-                    />
-                  </Container>
-                  <Container className="py-6">
-                    <RenderButtonType
-                      user={user}
-                      handleSignOut={handleSignOut}
-                      handleSignIn={googleSignIn}
-                    />
-                  </Container>
+      <Container
+        className={`fixed inset-y-0 right-0 lg:w-4/12 md:w-2/3 sm:w-6/12 bg-gray-900 z-50 transform transition-transform ease-in-out duration-150 ${sideBarOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        <Container className="bg-gray-100 h-screen">
+          <Container className="p-7">
+            <Container className="flex items-center justify-between">
+              <Logo />
+              <Button
+                type="button"
+                className="text-gray-700"
+                onClick={() => setSideBarOpen(false)}
+                btnText={
+                  <React.Fragment>
+                    <Text className="sr-only">Close menu</Text>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </React.Fragment>
+                }
+              />
+            </Container>
+            <Container className="mt-6">
+              <Container className="divide-y divide-gray-500/10">
+                <Container className="space-y-2 py-6">
+                  <NavBarOptions navigationData={navigationData} user={user} />
+                </Container>
+                <Container className="py-6">
+                  <RenderButtonType
+                    user={user}
+                    handleSignOut={handleSignOut}
+                    handleSignIn={googleSignIn}
+                  />
                 </Container>
               </Container>
             </Container>
           </Container>
         </Container>
-      )}
+      </Container>
     </div>
   )
 }
