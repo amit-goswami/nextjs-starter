e928d052edfc88bedc808ea571c3a23a98f096b9
am1t-kumar <thorragnarak@gmail.com>
2024-05-14 23:00:36 +0530

    fix: text alignment

--- src/features/home/components/best-seasonal-treks/components/BestSeasonalTreksCarousel.tsx
+++ src/features/home/components/best-seasonal-treks/components/BestSeasonalTreksCarousel.tsx
@@ -28,37 +28,38 @@ export const BestSeasonalTreksCarousel: React.FC<BestSeasonalTrekProps> = ({
       {bestTreksList?.treks &&
         bestTreksList.treks.map((trek, index) => {
           return (
-            <Link
-              href={ROUTES.TREK_PLANNER.replace(':id', trek.trek_id)}
-              key={index}
-            >
-              <Container
-                key={index}
-                className="relative flex-shrink-0 cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shaodw-lg"
-              >
-                <Container className="absolute z-10 inset-0 bg-gradient-to-b from-transparent via-transparent to-black" />
-                <Container className="absolute z-20 bottom-5 left-5 text-white">
-                  {trek.trek_name}
-                  <Container className="text-xs font-light flex items-center justify-between">
-                    <Container>
-                      <span className="text-xs font-light">{trek.state}</span>
-                      <span className="ml-2">{trek.altitude} Altitude</span>
-                      <span className="ml-2">|</span>
-                      <span className="ml-2 text-brand">{trek.days} days</span>
+            <Container className="min-w-[fit-content]" key={index}>
+              <Link href={ROUTES.TREK_PLANNER.replace(':id', trek.trek_id)}>
+                <Container
+                  key={index}
+                  className="relative flex-shrink-0 cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shaodw-lg"
+                >
+                  <Container className="absolute z-10 inset-0 bg-gradient-to-b from-transparent via-transparent to-black" />
+                  <Container className="absolute z-20 bottom-5 left-5 text-white">
+                    {trek.trek_name}
+                    <Container className="text-xs font-light flex items-center justify-between">
+                      <Container>
+                        <span className="text-xs font-light">{trek.state}</span>
+                        <span className="ml-2">{trek.altitude} Altitude</span>
+                        <span className="ml-2">|</span>
+                        <span className="ml-2 text-brand">
+                          {trek.days} days
+                        </span>
+                      </Container>
+                      {/* <span className="ml-2">₹ {trek.price} onwards</span> */}
                     </Container>
-                    {/* <span className="ml-2">₹ {trek.price} onwards</span> */}
                   </Container>
-                </Container>
-                <Image
-                  className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md 
+                  <Image
+                    className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md 
               shadow-gray-900 drop-shadow-xl dark:opacity-90"
-                  src={getRandomImage()}
-                  alt={trek.trek_id}
-                  width={1920}
-                  height={1080}
-                />
-              </Container>
-            </Link>
+                    src={getRandomImage()}
+                    alt={trek.trek_id}
+                    width={1920}
+                    height={1080}
+                  />
+                </Container>
+              </Link>
+            </Container>
           )
         })}
     </Container>
--- src/features/home/components/best-seasonal-treks/index.tsx
+++ src/features/home/components/best-seasonal-treks/index.tsx
@@ -17,11 +17,11 @@ export const BestSeasonalTreks = ({
   const { bestSeasonalTreks } = useHomeStore()
   return (
     <Container className="py-12" id="best-treks">
-      <Container className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Container className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <Container className="lg:text-center">
           <Text
             as="h2"
-            className="font-heading text-center mb-4 px-6 rounded-full w-fit md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font ring-2 ring-brand leading-7 dark:text-gray-600"
+            className="font-heading text-center mb-4 px-6 rounded-full md:w-72 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font ring-2 ring-brand leading-7 dark:text-gray-600"
           >
             Best treks in coming months
           </Text>
